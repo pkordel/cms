865b412c5ae53aed55a8de26be4f3e6746c88e03
Peter Kordel <pkordel@gmail.com>
2013-04-18 16:18:03 +0700

    Added state machine

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,22 @@
 class Article < ActiveRecord::Base
   hstore_accessor :metadata, :tag, :pronunciation, :etymology, :alternative_form
+
+  state_machine initial: :draft_in_progress do
+
+    event :submit do
+      transition :draft_in_progress => :pending_review
+    end
+
+    event :withdraw do
+      transition :pending_review => :draft_in_progress
+    end
+
+    event :approve do
+      transition :pending_review => :published
+    end
+
+    event :reject do
+      transition :pending_review => :rejected
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130417130817) do
+ActiveRecord::Schema.define(version: 20130418084518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20130417130817) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "type"
+    t.string   "state"
   end
 
   add_index "articles", ["metadata"], name: "articles_gin_metadata", using: :gin
